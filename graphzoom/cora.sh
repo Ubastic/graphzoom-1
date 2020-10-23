ff0a28bbfdefac27b002f9fbf1538a368f8b351d
super.kang <1689242311@qq.com>
2020-10-23 15:32:54 +0800

    single thread run

---
+++ graphzoom/cora.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for i in `seq 1 4`    ## coarsening level
+    do
+        ratio=$(case "$i" in
+            (1)  echo 2;;
+            (2)  echo 5;;
+            (3)  echo 9;;
+            (4)  echo 19;;
+        esac)
+        python graphzoom.py -r ${ratio} -m deepwalk -o lamg
+done
